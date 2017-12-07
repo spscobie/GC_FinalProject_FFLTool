7a29b1a37d8debbcae442b34af98a33cf9300a19
Jason Kubo <kuboj86@gmail.com>
2017-12-07 09:56:40 -0500

    sorting works mo better

--- GC_FinalProject_FFLTool/Controllers/HomeController.cs
+++ GC_FinalProject_FFLTool/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
+using System.IO; 
 using System.Net;
 using Newtonsoft.Json.Linq;
 using System.Text;
